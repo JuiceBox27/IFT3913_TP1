8d7a4bcb41d3756580c85dbdc115e2eda1a6ab46
JuiceBox27 <stefanroman@live.com>
2023-10-06 18:45:26 -0400

    Restructuring for jar creation (Not a big commit just too much refactoring :sweat_smile:).

---
+++ src/tp1/tropcomp.java
@@ -0,0 +1,25 @@
+package tp1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class tropcomp {
+    public static void main(String[] args) {
+        List<TestFile> tlsFiles = new ArrayList<TestFile>();
+        
+        Double limit = Double.parseDouble(args[3]);
+
+        tlsFiles = tls.execute(new String[] {args[2]});
+
+        tlsFiles.removeIf(t -> t.tcmp < limit);
+
+        System.out.println("path, package, class, tLOCs, assertions, tcmp");
+        for (TestFile testFile : tlsFiles) {
+            System.out.println(testFile.toString());
+        }
+
+        tls.createCSV(tlsFiles, args[1]);
+
+        System.out.println("Number of test files: " + tlsFiles.size());
+    }
+}
