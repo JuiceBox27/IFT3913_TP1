2095907017726b2b6fb0140034007f8cf043ed4a
JuiceBox27 <stefanroman@live.com>
2023-12-07 23:59:35 -0500

    Update aux tests.

--- currency-converter-in-java/src/test/CurrencyTest.java
+++ currency-converter-in-java/src/test/CurrencyTest.java
@@ -1,26 +1,69 @@
 package test;
 
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assume.assumeTrue;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
 import java.util.stream.Collectors;
 
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 
 import currencyConverter.Currency;
 
 public class CurrencyTest {
+
+    ArrayList<Currency> currencies;
+
+    Currency testCadCurrency;
+
+    List<String> testCurrencies;
+
+
+    @BeforeEach
+    void setUp() {
+        // testCurrencies = List.of("USD", "EUR", "GBP", "CHF", "CNY", "JPY");
+        testCurrencies = List.of("USD", "EUR", "GBP", "CHF", "CNY", "JPY", "CAD");
+
+        testCadCurrency = new Currency("CA Dollar", "CAD");
+
+        testCadCurrency.setExchangeValues("USD", 1.00);
+        testCadCurrency.setExchangeValues("EUR", 0.93);
+        testCadCurrency.setExchangeValues("GBP", 0.66);
+        testCadCurrency.setExchangeValues("CHF", 1.01);
+        testCadCurrency.setExchangeValues("CNY", 6.36);
+        testCadCurrency.setExchangeValues("JPY", 123.54);
+
+        currencies = Currency.init();
+        currencies.add(testCadCurrency);
+    }
+
+    // A black box domain test and limit values.
     @Test
     public void testConvert() {
         double[] amounts = {-1000000, -1, 0, 1, 500000, 1000000, 1000001, 2000000};
-        // double[] amounts = {0, 1, 500000, 1000000};
-        double testValue = Currency.convert(1d, 2d);
 
         // Verify the conversion is accurate and precise.
         assertAll(
             () -> Arrays.stream(amounts)
                         .forEach(a -> assertEquals(Currency.convert(a, 2.0d), a * 2.0d))
         );
-    } 
+    }
+    
+    // A black box test of the types of currencies accepted.
+    // @Test
+    // public void testCurrencies() {
+
+    //     assertAll(
+    //         () -> testCurrencies.stream().forEach(tc -> 
+    //                 assertTrue(currencies.stream().map(c -> 
+    //                     c.getShortName()).collect(Collectors.toList()).contains(tc)))
+    //     );
+    // }
 }
--- currency-converter-in-java/src/test/MainWindowTest.java
+++ currency-converter-in-java/src/test/MainWindowTest.java
@@ -1,10 +1,52 @@
 package test;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import currencyConverter.Currency;
+import currencyConverter.MainWindow;
+
 public class MainWindowTest {
+
+    ArrayList<Currency> actualCurrencies;
+
+    Currency testCadCurrency;
+
+    @BeforeEach
+    void setUp() {
+        testCadCurrency = new Currency("CA Dollar", "CAD");
+
+        testCadCurrency.setExchangeValues("USD", 1.00);
+        testCadCurrency.setExchangeValues("EUR", 0.93);
+        testCadCurrency.setExchangeValues("GBP", 0.66);
+        testCadCurrency.setExchangeValues("CHF", 1.01);
+        testCadCurrency.setExchangeValues("CNY", 6.36);
+        testCadCurrency.setExchangeValues("JPY", 123.54);
+
+        actualCurrencies = Currency.init();
+        actualCurrencies.add(testCadCurrency);
+    }
+
     @Test
     void testConvert() {
-        
+        assertNotNull(actualCurrencies);
+        assertEquals(7, actualCurrencies.size());
+
+
+    }
+
+    @Test
+    void testConvertBlackBox() {
+
+    }
+
+    @Test
+    void testConvertWhiteBox() {
+        MainWindow.convert("USD", "EUR", actualCurrencies, 100.0d);
     }
 }
