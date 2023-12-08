822a70d93d64e967c20d2086e384923c1df73eb6
JuiceBox27 <stefanroman@live.com>
2023-12-08 16:16:59 -0500

    FlowChart udpate and blackbox test for mainwindow updated for currency values.

--- currency-converter-in-java/src/test/MainWindowTest.java
+++ currency-converter-in-java/src/test/MainWindowTest.java
@@ -1,7 +1,6 @@
 package test;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
 
 import java.util.ArrayList;
 
@@ -13,40 +12,70 @@ import currencyConverter.MainWindow;
 
 public class MainWindowTest {
 
-    ArrayList<Currency> actualCurrencies;
+    ArrayList<Currency> appCurrencies;
+    ArrayList<Currency> allCurrencies;
 
     Currency testCadCurrency;
+    Currency testAudCurrency;
+
+    double testAmount = 100.07d;
 
     @BeforeEach
     void setUp() {
-        testCadCurrency = new Currency("CA Dollar", "CAD");
-
-        testCadCurrency.setExchangeValues("USD", 1.00);
-        testCadCurrency.setExchangeValues("EUR", 0.93);
-        testCadCurrency.setExchangeValues("GBP", 0.66);
-        testCadCurrency.setExchangeValues("CHF", 1.01);
-        testCadCurrency.setExchangeValues("CNY", 6.36);
-        testCadCurrency.setExchangeValues("JPY", 123.54);
-
-        actualCurrencies = Currency.init();
-        actualCurrencies.add(testCadCurrency);
+        setUpCurrencies();
     }
 
-    @Test
-    void testConvert() {
-        assertNotNull(actualCurrencies);
-        assertEquals(7, actualCurrencies.size());
-
-
-    }
-
+    
     @Test
     void testConvertBlackBox() {
+        assertEquals((testAmount * 0.93d), MainWindow.convert("US Dollar", "Euro", appCurrencies, testAmount), 0.01d);
+        assertEquals(0, MainWindow.convert("US Dollar", "AU Dollar", appCurrencies, testAmount), 0.01d);
+        assertEquals(0, MainWindow.convert("CA Dollar", "Euro", appCurrencies, testAmount), 0.01d);
+        assertEquals(0, MainWindow.convert("CA Dollar", "Euro", appCurrencies, testAmount), 0.01d);
 
+        assertEquals((testAmount * 0.93d), MainWindow.convert("US Dollar", "Euro", allCurrencies, testAmount), 0.01d);
+        assertEquals((testAmount * 88.00d), MainWindow.convert("US Dollar", "AU Dollar", allCurrencies, testAmount), 0.01d);
+        assertEquals((testAmount * 22.00d), MainWindow.convert("CA Dollar", "Euro", allCurrencies, testAmount), 0.01d);
+        assertEquals((testAmount * 33.00d), MainWindow.convert("CA Dollar", "AU Dollar", allCurrencies, testAmount), 0.01d);
+
+        
     }
 
     @Test
     void testConvertWhiteBox() {
-        MainWindow.convert("USD", "EUR", actualCurrencies, 100.0d);
+        MainWindow.convert("USD", "EUR", appCurrencies, 100.0d);
+    }
+
+    private void setUpCurrencies() {
+        testCadCurrency = new Currency("CA Dollar", "CAD");
+        testAudCurrency = new Currency("AU Dollar", "AUD");
+
+        testCadCurrency.setExchangeValues("USD", 11.00);
+        testCadCurrency.setExchangeValues("EUR", 22.00);
+        testCadCurrency.setExchangeValues("GBP", 44.00);
+        testCadCurrency.setExchangeValues("CHF", 44.00);
+        testCadCurrency.setExchangeValues("CNY", 44.00);
+        testCadCurrency.setExchangeValues("JPY", 44.00);
+        testCadCurrency.setExchangeValues("AUD", 33.00);
+
+        testAudCurrency.setExchangeValues("USD", 11.00);
+        testAudCurrency.setExchangeValues("EUR", 22.00);
+        testAudCurrency.setExchangeValues("GBP", 44.00);
+        testAudCurrency.setExchangeValues("CHF", 44.00);
+        testAudCurrency.setExchangeValues("CNY", 44.00);
+        testAudCurrency.setExchangeValues("JPY", 44.00);
+        testAudCurrency.setExchangeValues("CAD", 33.00);
+
+        allCurrencies = Currency.init();
+        appCurrencies = Currency.init();
+
+        allCurrencies.forEach(
+            c -> {
+                c.setExchangeValues("CAD", 99.00);
+                c.setExchangeValues("AUD", 88.00);
+            });
+
+        allCurrencies.add(testCadCurrency);
+        allCurrencies.add(testAudCurrency);
     }
 }
